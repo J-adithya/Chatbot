dc99a49acd796efbb0cca63c6bc375edcf0856c5
J-adithya <162279108+J-adithya@users.noreply.github.com>
2024-06-06 12:20:51 +0530

    Add files via upload

---
+++ Prototype 2/guichat.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from tkinter import *
+from transformers import BlenderbotForConditionalGeneration, BlenderbotTokenizer
+
+# Load the tokenizer and model
+model_name = "facebook/blenderbot-400M-distill"
+tokenizer = BlenderbotTokenizer.from_pretrained(model_name)
+model = BlenderbotForConditionalGeneration.from_pretrained(model_name)
+
+def chatbot_response(msg):
+    inputs = tokenizer([msg], return_tensors="pt")
+    reply_ids = model.generate(**inputs, max_length=100, num_return_sequences=1, temperature=0.9)
+    reply = tokenizer.decode(reply_ids[0], skip_special_tokens=True)
+    return reply
+
+def send():
+    msg = EntryBox.get("1.0", 'end-1c').strip()
+    EntryBox.delete("0.0", END)
+
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Verdana", 12))
+
+        res = chatbot_response(msg)
+        ChatLog.insert(END, "Bot: " + res + '\n\n')
+
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+base = Tk()
+base.title("ChatBot")
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+
+# Create Chat window
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog.config(state=DISABLED)
+
+# Bind scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+# Create Button to send message
+SendButton = Button(base, font=("Verdana", 12, 'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b", fg='#ffffff',
+                    command=send)
+
+# Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
+
+# Place all components on the screen
+scrollbar.place(x=376, y=6, height=386)
+ChatLog.place(x=6, y=6, height=386, width=370)
+EntryBox.place(x=6, y=401, height=90, width=265)
+SendButton.place(x=282, y=401, height=90)  # Placing send button on the right side
+
+base.mainloop()
